d4e7184bc1733e719d110a0645a08ed94bf1df3f
Melanie Bousquet <bousquet.melanie@gmail.com>
2017-02-21 23:06:17 +0100

    Ajout fichiers html css js

---
+++ slideshow.js
@@ -0,0 +1,96 @@
+(function ($) {
+    $.fn.slideshow = function (number) {
+
+
+        var Slider = {
+            currentIndex: 0,
+
+            init: function () {
+                Slider.autoSlide();
+                Slider.playAutoClick();
+                Slider.nextSlideOnClick();
+                Slider.prevSlideOnClick();
+                Slider.changeSlideOnKeypress();
+            },
+
+            activeSlide: function () {
+                var slides = $('.fade');
+                var slide = slides.eq(Slider.currentIndex);
+                slides.hide();
+                slide.css('display', 'flex');
+            },
+
+            indexPlus: function () {
+                var slides = $('.fade');
+                var slidesNumber = slides.length;
+                Slider.currentIndex += 1;
+                if (Slider.currentIndex > slidesNumber - 1) {
+                    Slider.currentIndex = 0;
+                }
+            },
+
+            indexMinus: function () {
+                var slides = $('.fade');
+                var slidesNumber = slides.length;
+                Slider.currentIndex -= 1;
+                if (Slider.currentIndex < 0) {
+                    Slider.currentIndex = slidesNumber - 1;
+                }
+            },
+
+            autoSlide: function () {
+                var play = $('.play');
+                play.click(function () {
+                    var timer = setInterval(function () {
+                        Slider.indexPlus();
+                        Slider.activeSlide();
+                    }, number);
+                    var stop = $('.stop');
+                    stop.click(function () {
+                        clearInterval(timer);
+                    });
+                });
+
+            },
+
+            playAutoClick: function () {
+                var play = $('.play');
+                play.trigger('click');
+            },
+
+            nextSlideOnClick: function () {
+                var next = $('.next');
+                next.click(function () {
+                    Slider.indexPlus();
+                    Slider.activeSlide();
+                });
+            },
+
+            prevSlideOnClick: function () {
+                var prev = $('.prev');
+                prev.click(function () {
+                    Slider.indexMinus();
+                    Slider.activeSlide();
+                });
+            },
+
+            changeSlideOnKeypress: function () {
+                $('body').keydown(function (e) {
+                    if (e.which === 39) {
+                        Slider.indexPlus();
+                        Slider.activeSlide();
+                    } else if (e.which === 37) {
+                        Slider.indexMinus();
+                        Slider.activeSlide();
+                    }
+                })
+            },
+        }
+
+        Slider.init();
+
+    };
+
+})(jQuery);
+
+
